8d9f5667f64b2470a1402b429205a3e33f85df32
Dana <daniela@ilogica.cl>
2016-04-28 21:30:57 -0300

    add persoanl data to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427233019) do
+ActiveRecord::Schema.define(version: 20160429002918) do
 
   create_table "quotes", force: :cascade do |t|
     t.text     "content"
@@ -32,13 +32,14 @@ ActiveRecord::Schema.define(version: 20160427233019) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role"
     t.string   "name"
     t.string   "lastname"
     t.string   "username"
-    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
