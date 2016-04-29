3450949c664f44477f5c839f3c7f166764ead22b
Rodrigo <hein.rodrigo@gmail.com>
2016-04-28 22:11:57 -0300

    tag model deleted, schema fixed (grande juancri)

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429002918) do
+ActiveRecord::Schema.define(version: 20160429004719) do
 
   create_table "quotes", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "author"
   end
 
   create_table "users", force: :cascade do |t|
