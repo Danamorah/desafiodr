c759f825b9fa7a88b335080e8ae0c9cfa39ec03d
Rodrigo <hein.rodrigo@gmail.com>
2016-05-19 21:23:19 -0300

    gsub added to incorrect_quote in gameplay/match

--- app/controllers/gameplay_controller.rb
+++ app/controllers/gameplay_controller.rb
@@ -1,51 +1,21 @@
 class GameplayController < ApplicationController
-  before_action :set_opponent, only:[:round]
-  before_action :set_correct, only:[:match]
-  before_action :set_quote, only:[:match]
-#  before_action :set_incorrect, only:[:match]
-
+  # before_action :set_opponent, only:[:round]
+  # before_action :set_correct, only:[:match]
+  # before_action :set_quote, only:[:match]
+  # before_action :set_incorrect, only:[:match]
   def game_room
-    #aprete new game, redirigir a round
   end
 
   def round
-    #va a llamar a contrincante (user, bot)
-    #next -> redirigir a match
+     @opponent = User.all.sample
   end
 
   def match
-    #va a llamar correctas (segun nivel)
-    #luego, quotes que contengan esa palabra correcta
-    #muestra la quote con su version incorrecta de esa palabra
     #jugabilidad
     #guardar resultados de matches 1 y 2
-  end
-
-private
-
-  def set_opponent
-    @opponent ||= User.all.sample
-  end
-
-  def set_quote
-    @quotes = Quote.search_by_word(@correct).sample
-  end
-
-  def set_correct
     @correct = Correct.all.sample
+    @quote = Quote.search_by_word(@correct).sample
+    @incorrect = @correct.incorrects.sample
+    @incorrect_quote = @quote.content.gsub(@correct.word, @incorrect.word)
   end
-
-  def set_incorrect
-#    @incorrect = 
-  end
-
-  def round_2
-  end
-
-  def match_1
-  end
-
-  def match_2
-  end
-
 end
