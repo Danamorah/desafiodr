061121575f06e836c5042f884db158805acbb29c
Danamorah <damoraherrera@gmail.com>
2016-05-24 22:08:59 -0400

    add submit buttom

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,11 +5,37 @@ $(document).ready(function() {
     });
 
     $('.quote span').on('click', '', function(){
-    	if ($(this).attr('data-info') === '1') {
-    		$(this).attr('contenteditable', 'true');
-    	} else {
-    		//alert('game over');
-    	}
+        if ($(this).attr('data-info') === '1') {
+            $(this).attr('contenteditable', 'true').addClass("edit");;
+        } else {
+            //alert('game over');
+        }
     });
+
+    $('.send_quote').on('click', '', function(){
+        var get_quote = $('.quote').text();
+        var g_quote = get_quote.replace(/\s(?=\s)/g,'');
+        console.log(g_quote);
+    });
+
+
+   // submit answer //
+   /*$( ".edit" ).keypress(function( event ) {
+    if ( event.which == 13 && event.shiftKey != 1) {
+        event.preventDefault();
+        var txt = $(".edit").html();
+        $('span').append("");   
+        $('span').submit();
+    }
+});*/
+   
+   $(".edit").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        $("form").submit();
+    }
+
+});
+
 });
 
